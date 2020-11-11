12b72e7066f8e431d8c92c948997303376982d69
Martin <s182935@student.dtu.dk>
2020-11-11 11:24:58 +0100

    Player Class

---
+++ src/main/java/Player.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Player{
+    private String name;
+    private Boolean inJail;
+    private Boolean JailCard;
+    private Boolean[] myProperties = new Boolean[12]; //spilleren ejendomme
+    private int Position;
+    private int  playernumber;
+
+
+    public Player(String Name, int number)
+    {
+        name = Name;
+        inJail = false;
+        JailCard = false;
+        Position = 0;
+        playernumber = number;
+    }
+
+    public int getplayernumber()
+    {
+        return playernumber;
+    }
+
+    public int getPosition()
+    {
+        return Position;
+    }
+    public void setPosition(int properties)
+    {
+        this.Position = Position;
+    }
+    public void setMyProperties(int k)
+    {
+        myProperties[k] = true;
+    }
+    public void setJailCard(boolean k) {
+        if (!k)
+            JailCard = false;
+        else JailCard = true;
+    }
+    //Return array with players properties
+    public boolean getmyProperties(int k){
+        return myProperties[k];
+    }
+
+    Wallet wallet = new Waller();
+}
