0aef04f6996bf0f7c546e1b7151a839057b5fcf9
Sealnpenguin <sealnpenguin@protonmail.com>
2020-11-11 19:57:00 +0100

    testede om GUI_Jail gjorde det forventede

--- src/main/java/UIController.java
+++ src/main/java/UIController.java
@@ -1,4 +1,5 @@
 import gui_fields.GUI_Field;
+import gui_fields.GUI_Jail;
 import gui_fields.GUI_Start;
 
 import java.awt.*;
@@ -8,7 +9,13 @@ public class UIController {
     public GUI_Field[] UIFieldGen(int test){
         GUI_Field[] guiFields = new GUI_Field[test];
         for (int i = 0; i < guiFields.length; i++) {
-            guiFields[i] = new GUI_Start("Start", "Modtag: 200", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK); {
+            //Stub code to see if it works as expected
+            if(i == 18){
+                guiFields[i] = new GUI_Jail("default", "Fængsel", "Fængsel", "På besøg i fængslet", new Color(125, 125, 125), Color.BLACK);
+            }
+            else {
+                guiFields[i] = new GUI_Start("Start", "Modtag: 200", "Modtag kr. 200,-\nnår de passerer start", Color.RED, Color.BLACK);
+
             }
         }
         return guiFields;
