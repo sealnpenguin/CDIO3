5499d6bbc4fd4e07405d50ff4ade5a9f18893ad7
ASBP <s194582@student.dtu.dk>
2020-11-12 19:04:41 +0100

    Forberedelse til udviklingen af Field.

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,7 +1,20 @@
 public class Field {
+    //Die for development purposes (from CDIO 2)
+    private static int MAX = 6; // max value of a die
+    private static int faceValue; // value of the die
+    public static int rollDice() {
+        faceValue = (int)(Math.random() * MAX) +1;
+        return faceValue;
+    }
     String[] allFields;
-
-    public Field(String[] field){
+    public Field(String[]field){
         this.allFields = field;
+    }
+
+    public static void play() { //working title
+        rollDice();
+        System.out.println(faceValue);
+
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@ import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        gui.displayChanceCard("test");
+        //GUI gui = new GUI();
+        //gui.displayChanceCard("test");
+
+        Field.play();
     }
 
 }
