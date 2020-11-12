19ddef28f77f77d311da8074c9be90b3b345c89f
ASBP <s194582@student.dtu.dk>
2020-11-12 21:22:52 +0100

    Ejendomme til UI. Højst ufærdigt.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,66 +13,70 @@ public class Main {
 
 
 
-        ///////////////////////Tilhører Properties, når "gui" oprettes et andet sted, så man kan nedarve?
-        //GUI_Field field;
+
+
+
+        /////////////////////// Tilhører Properties ////////////////
+        GUI_Field field;
 
         // Tilgår start feltet - hentet fra github UI guide
-        //GUI_Field[] fields = gui.getFields();
-        //field = fields[0];
+        GUI_Field[] fields = gui.getFields();
+        field = fields[0];
 
-
-
-
-
-
-        // Opretter spiller
-        GUI_Player player = new GUI_Player("Stephen", 2000);
-        gui.addPlayer(player);
-
-// Henter feltet
-        GUI_Field field = gui.getFields()[4];
-
-// Sæt bilen til at blive vist
-        field.setCar(player, true);
         //Field.play();
 
         // Cast felt til GUI_Ownable
-        //field = gui.getFields()[1];
-        GUI_Ownable ownable = (GUI_Ownable) field;
+        //GUI_Ownable ownable = (GUI_Ownable) field;
+        //ownable.setSubText("Pris: 1000");
+        //field.setBackgroundColor(Color.WHITE);
 
+        field = gui.getFields()[1];
+        field.setTitle("Burgerbaren");
+        field.setSubText("M1");
 
-        for (int i = 0; i < 24; i++ ) {
-            field = gui.getFields()[i];
-            ownable = (GUI_Ownable) field;
-            if (i == 0){
-                ownable.setSubText("Pris: 1000");
-                //field.setBackgroundColor(Color.WHITE);
-            }
-            switch (i) {
-                case 1 -> {
-                    field.setTitle("Burgerbaren");
-                    ownable.setSubText("M1");
-                }
-                case 2 -> {
-                    field.setTitle("Pizzahuset");
-                    ownable.setSubText("M1");
-                }
-                case 3 -> field.setTitle("Chance");
-                case 4 -> {
-                    field.setTitle("Godtebutikken");
-                    ownable.setSubText("M1");
-                }
-                case 5 -> {
-                    field.setTitle("Iskiosken");
-                    ownable.setSubText("M1");
-                }
-                case 6 -> ownable.setSubText("Pris: 1000");
-                case 7 -> ownable.setSubText("Pris: 1000");
-                case 8 -> ownable.setSubText("Pris: 1000");
-                case 9 -> field.setTitle("Chance");
-            }
+        field = gui.getFields()[2];
+        field.setTitle("Pizzahuset");
+        field.setSubText("M1");
 
-        }
+        field = gui.getFields()[3];
+        field.setTitle("Chance");
+        field.setSubText("");
+
+        field = gui.getFields()[4];
+        field.setTitle("Godtebutikken");
+        field.setSubText("M1");
+
+        field = gui.getFields()[5];
+        field.setTitle("Iskiosken");
+        field.setSubText("M1");
+
+        field = gui.getFields()[6];
+        field.setTitle("I fængsel");
+        field.setSubText("På besøg");
+
+        field = gui.getFields()[5];
+        field.setTitle("Museet");
+        field.setSubText("M2");
+
+        field = gui.getFields()[6];
+        field.setTitle("Biblioteket");
+        field.setSubText("M2");
+
+        field = gui.getFields()[7];
+        field.setTitle("Chance");
+
+        field = gui.getFields()[8];
+        field.setTitle("Rullebrætparken");
+        field.setSubText("M2");
+
+        field = gui.getFields()[9];
+        field.setTitle("Svømmebassinet");
+        field.setSubText("M2");
+
+        field = gui.getFields()[10];
+        field.setTitle("Gratis");
+        field.setSubText("Parkering");
+
     }
 
 }
--- src/main/java/Properties.java
+++ src/main/java/Properties.java
@@ -1,5 +1,6 @@
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Ownable;
+import gui_fields.GUI_Player;
 import gui_main.GUI;
 
 import java.awt.*;
