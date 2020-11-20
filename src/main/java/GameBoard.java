6f5efb334368f7fde1ba24d92f9367e789425bc0
zPadduz <jul2009@live.dk>
2020-11-20 15:48:40 +0100

    Har oprettet muligheden for at vælge sprog

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -11,6 +11,7 @@ public class GameBoard {
     static void Game() {
         int endGameIf = 0;
         boolean GameOver = false;
+        String lang;
         Die die = new Die(1); // One die is instantiated with new Dice(int X);
 
 
@@ -18,19 +19,23 @@ public class GameBoard {
         Field[] myFields = f1.getFieldArr();
         UIController uiController = new UIController(myFields);
 
+        lang = uiController.getGUI().getUserButtonPressed("" ,"English", "Dansk");
+        Language langSelector = new Language(lang);
+        String[] currentLang = langSelector.returnLang();
 
 
 
-        int numberOfPlayers = uiController.getGUI().getUserInteger("Enter the number of players ranging from 2-4");
+
+        int numberOfPlayers = uiController.getGUI().getUserInteger(currentLang[0]);
         while (numberOfPlayers > 4 || numberOfPlayers < 2) {
-            uiController.getGUI().showMessage("Please try again in the range of 2-4");
-            numberOfPlayers = uiController.getGUI().getUserInteger("Enter the number of players ranging from 2-4");
+            uiController.getGUI().showMessage(currentLang[1]);
+            numberOfPlayers = uiController.getGUI().getUserInteger(currentLang[0]);
         }
         Player[] playerList = new Player[numberOfPlayers];
         // Create player objects as per the playerdefined numberOfPlayers int.
 
         for (int i = 1; i < numberOfPlayers+1; i++) {
-            Player player = new Player(uiController.getGUI().getUserString("Enter name for player " + i));
+            Player player = new Player(uiController.getGUI().getUserString(currentLang[2] + i));
             //Sets the players money according the rules
             switch (numberOfPlayers) {
                 case 2 -> player.setMoney(20);
@@ -43,16 +48,30 @@ public class GameBoard {
         //Initialize and Add players to gui
         for (int i = 0; i < playerList.length; i++) {
             //
-            String color = uiController.getGUI().getUserSelection(uiController.getGuiPlayer(i).getName()+" choose a color for your car","RED", "BLACK", "BLUE","MAGENTA","PINK","CYAN","YELLOW","WHITE");
-            switch (color) {
-                case "RED" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.RED);
-                case "BLACK" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.BLACK);
-                case "BLUE" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.BLUE);
-                case "MAGENTA" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.MAGENTA);
-                case "PINK" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.PINK);
-                case "CYAN" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.CYAN);
-                case "YELLOW" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.YELLOW);
-                case "WHITE" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.WHITE);
+            String color = uiController.getGUI().getUserSelection(uiController.getGuiPlayer(i).getName()+currentLang[3],currentLang[4], currentLang[5],currentLang[6],currentLang[7],currentLang[8],currentLang[9],currentLang[10],currentLang[11]);
+            if(lang.equals("English")) {
+                switch (color) {
+                    case "RED" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.RED);
+                    case "BLACK" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.BLACK);
+                    case "BLUE" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.BLUE);
+                    case "MAGENTA" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.MAGENTA);
+                    case "PINK" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.PINK);
+                    case "CYAN" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.CYAN);
+                    case "YELLOW" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.YELLOW);
+                    case "WHITE" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.WHITE);
+                }
+            }
+            if(lang.equals("Dansk")) {
+                switch (color) {
+                    case "RØD" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.RED);
+                    case "SORT" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.BLACK);
+                    case "BLÅ" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.BLUE);
+                    case "MAGENTA" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.MAGENTA);
+                    case "ROSE" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.PINK);
+                    case "CYAN" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.CYAN);
+                    case "GUL" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.YELLOW);
+                    case "HVID" -> uiController.getGuiPlayer(i).getCar().setPrimaryColor(Color.WHITE);
+                }
             }
             // we update GUIPLAYERPOS here to set player at start
             uiController.updateGUIPlayerPos(playerList[i],playerList[i].getOldposition(), playerList[i].getPosition());
@@ -70,7 +89,7 @@ public class GameBoard {
                     if (playerList[k].getMoney() <= endGameIf)
                     {
                         GameOver = true;
-                        uiController.getGUI().showMessage("Gameover! "+ playerList[k].getName() + " has reached 0 and lost the game");
+                        uiController.getGUI().showMessage("Gameover! "+ playerList[k].getName() + currentLang[12]);
                         int[] a = new int[numberOfPlayers];
                         int max = 0;
 
@@ -84,7 +103,7 @@ public class GameBoard {
                         }
                         // second loop to announce winner
                         for (int j = 0; j < a.length; j++) {
-                            if(playerList[j].getMoney() == max) uiController.getGUI().showMessage("And the winner is...  "+ playerList[j].getName() + " !!!");
+                            if(playerList[j].getMoney() == max) uiController.getGUI().showMessage(currentLang[13]+ playerList[j].getName() + " !!!");
                         }
                         break;
                     }
@@ -94,9 +113,9 @@ public class GameBoard {
                 //gui.showMessage(playerList.get(i).getName() + " has the die in his court");
 
                 //Guibutton to read the next user input
-                String ready = uiController.getGUI().getUserButtonPressed(uiController.getGuiPlayer(i).getName() + " press button to roll the die!", "Roll");
+                String ready = uiController.getGUI().getUserButtonPressed(uiController.getGuiPlayer(i).getName() + currentLang[14], currentLang[15]);
                 // if statement to check if the user typed in throw
-                if (ready.equals("Roll")) {
+                if (ready.equals(currentLang[15])) {
                     //Change die on in gui to reflect new roll and update player position
                     uiController.getGUI().setDie(die.rollDie());
                     playerList[i].setPosition(+die.getFaceValue());
