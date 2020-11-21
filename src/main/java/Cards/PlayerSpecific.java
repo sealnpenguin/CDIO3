ea21a1ebf164bf8b851847a47903d48633c133f6
Sealnpenguin <sealnpenguin@protonmail.com>
2020-11-21 13:29:39 +0100

    Har implementeret en del af "PlayerSpecific" kort. Kat/Hund osv. Har gjordt så de gives videre til den rigtige spiller. Mangler dog kald tilbage til GUI så man trækker et kort mere

--- src/main/java/Cards/PlayerSpecific.java
+++ src/main/java/Cards/PlayerSpecific.java
@@ -13,7 +13,22 @@ public class PlayerSpecific extends Cards
     public void drawCard(Player[] players, int player, Field[] fields)
     {
         switch (this.getCardText().substring(0,2)){
+            //Bilkort
             case("1:"):
+                players[0].getCardArray().add(this);
+                break;
+            //Skibskort
+            case("6:"):
+                players[1].getCardArray().add(this);
+                break;
+            //Kattekort
+            case("12:"):
+                players[2].getCardArray().add(this);
+                break;
+            //Hundekort
+            case("13:"):
+                players[3].getCardArray().add(this);
+                break;
 
         }
 
