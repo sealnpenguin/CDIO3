23b80f9c78d84cd9db54f8699d39b5b9e5a6aa96
SAA.DK <sebastian_almfort@hotmail.com>
2020-11-21 13:00:38 +0100

    Skateparken er færdig

--- src/main/java/Cards/SpecificField.java
+++ src/main/java/Cards/SpecificField.java
@@ -16,7 +16,15 @@ public class SpecificField extends Cards
         switch(this.getCardText().substring(0, 2))
         {
             case("18"):
+                Properties p1 = ((Properties)fields[10]);
                 players[player].setSpecificPosition(10);
+                if(p1.getOwnedBy() == -1 && p1.getOwnedBy() == player){
+                    p1.setOwnedBy(player);
+                }
+                else{
+                    players[player].setMoney(-p1.getPrice());
+                    players[p1.getOwnedBy()].setMoney(p1.getPrice());
+                }
 
 
                 //Plads 10
