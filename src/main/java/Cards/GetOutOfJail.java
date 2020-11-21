97b67ab0c2c8d6bd67686c4aa23effab4e2ea067
SAA.DK <sebastian_almfort@hotmail.com>
2020-11-21 11:00:49 +0100

    Subclasses i Cards

---
+++ src/main/java/Cards/GetOutOfJail.java
@@ -0,0 +1,15 @@
+package Cards;
+
+public class GetOutOfJail extends Cards
+{
+    public GetOutOfJail(String text, int index)
+    {
+        super(text,index);
+    }
+
+    @Override
+    public void drawCard(Player[] players)
+    {
+
+    }
+}
