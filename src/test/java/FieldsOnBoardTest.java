52f4e66a42ad4f27fb7517b5d0f72a4edde39b86
Sealnpenguin <sealnpenguin@protonmail.com>
2020-11-21 14:14:33 +0100

    tilføjet PlayerTest og FieldsOnBoardTest til git

---
+++ src/test/java/FieldsOnBoardTest.java
@@ -0,0 +1,20 @@
+import gui_main.GUI;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldsOnBoardTest {
+
+    @Test
+    void fieldsOnBoard() {
+        FieldsOnBoard f1 = new FieldsOnBoard();
+        f1.fieldsOnBoard();
+        for (int i = 0; i < f1.getFieldArr().length; i++) {
+            Field[] testarr = f1.getFieldArr();
+            String testString = testarr[i].getFieldName();
+            System.out.println(testString + " fieldOnBoardtest test");
+        }
+        UIController myControl = new UIController(f1.getFieldArr());
+        myControl.getGUI().getUserButtonPressed("test", "Button1");
+    }
+}
---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+}
