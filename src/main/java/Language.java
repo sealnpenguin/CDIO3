fd0e71bbd381fc6327263eddd0a3d8c1422e61eb
zPadduz <jul2009@live.dk>
2020-11-23 18:41:48 +0100

    Opdateret Language til at stemme overens med chancekort

--- src/main/java/Language.java
+++ src/main/java/Language.java
@@ -6,13 +6,13 @@ public class Language {
     String[] dk = {"Indtast antallet af spillere fra 2-4", "Prøv igen i intervallet 2-4", "Indtast navn til spiller ", " vælg en farve til din bil",
             "RØD", "SORT", "BLÅ", "MAGENTA", "ROSE", "CYAN", "GUL", "HVID", " har 0M og taber spillet", "Og vinderen er ... "," tryk på knappen for at kaste terningen!",
             "Kast", "Spil igen?", "Ja!", "Nej!", "To spillere med samme navn er ikke tilladt! Prøv igen spiller ", " er i fængsel og betaler 1M for at blive løsladt", " bruger sit fængselskort og løslades uden omkostninger"
-            ,": Hunden ", ": Katten ", ": Bilen ", ": Skibet "};
+            ,": Bilen ", ": Skibet ", ": Katten ", ": Hunden "};
 
     //array that store strings in english
     String[] eng = {"Enter the number of players ranging from 2-4", "Please try again in the range of 2-4", "Enter name for player ", " choose a color for your car",
             "RED", "BLACK", "BLUE","MAGENTA","PINK","CYAN","YELLOW","WHITE", " has reached 0 and lost the game", "And the winner is...  ", " press button to roll the die!",
             "Roll", "Play Again?", "Yes!", "No!", "Two players with the same name isn't allowed! Try again player ", " is in jail and pays 1M to get released", " uses their jailcard and gets released for free"
-            ,": The Dog" , ": The Cat", ": The Car", ": The Ship"};
+            ,": The Car" , ": The Ship", ": The Cat", ": The Dog"};
 
 
     public Language(String lang) {
